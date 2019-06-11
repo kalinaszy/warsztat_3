6fb30386759b480b8bd461c9e340087c503e1b53
Kalina <kalina.szymczyk@gmail.com>
2019-06-12 00:44:36 +0200

    models

--- core/models.py
+++ core/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-# Create your models here.
-# Jest to reprezentacja sali w aplikacji. Powinna przechowywać następujące wartości: nazwa, pojemność, dostępność rzutnika.
-# Rezerwacja
-#
-# Ma reprezentować rezerwację sali na dany dzień. Powinien przechowywać następujące dane: datę, id sali, komentarz dodany
-# przy rezerwacji.
-# Powinna być połączona z obiektem Sala relacją: jedna sala może mieć wiele całodniowych rezerwacji, każdą innego dnia.
 class Room(models.Model):
     name = models.CharField(max_length=255, unique=True, null=False)
     capacity = models.IntegerField()
